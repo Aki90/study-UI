be712f5f2c3131860c6c96336e3f37ca556fa6d5
aki90 <jjshh9095@naver.com>
2023-05-05 04:49:06 +0900

    product carousel 마크업

---
+++ js/productcarousel.js
@@ -0,0 +1,8 @@
+const slider = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  mouseDrag: true,
+});
