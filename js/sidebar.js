cb18ca65ff68264129927753afb5ad96b72b7315
aki90 <jjshh9095@naver.com>
2023-05-07 16:25:58 +0900

    Sidebar 메뉴 토글

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,2 +1,18 @@
-const menuButton = document.querySelector('.gnb-icon-button');
-console.log(menuButton);
+window.addEventListener('load', () => {
+  const menuButton = document.querySelector('.gnb-icon-button');
+  const sidebar = document.querySelector('.sidebar');
+  const sidebarOverlay = document.querySelector('.overlay');
+
+  function openSidebar() {
+    sidebar.classList.add('is-active');
+    sidebarOverlay.classList.add('is-active');
+  }
+
+  function closeSidebar() {
+    sidebar.classList.remove('is-active');
+    sidebarOverlay.classList.remove('is-active');
+  }
+
+  menuButton.addEventListener('click', openSidebar);
+  sidebarOverlay.addEventListener('click', closeSidebar);
+});
