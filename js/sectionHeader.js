a7acfff8e3d10a53803330d227f909bf7c2e5ad1
aki90 <jjshh9095@naver.com>
2023-05-07 17:06:51 +0900

    모바일 Section Header

---
+++ js/sectionHeader.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  const sectionHeaderIconButton = document.querySelector(
+    '.product-shipment .product-section-header.sm-only .icon-button'
+  );
+
+  function showFullSection() {
+    const section = this.parentNode.parentNode;
+    section.classList.add('is-open');
+  }
+
+  sectionHeaderIconButton.addEventListener('click', showFullSection);
+});
