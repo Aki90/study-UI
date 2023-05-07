e1d46919fb362dd706676410e11f5c975d4873de
aki90 <jjshh9095@naver.com>
2023-05-07 16:50:20 +0900

    Sidebar Drawer 토글

---
+++ js/sidebarDrawer.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+  const drawerMenuButtonList = document.querySelectorAll(
+    '.sidebar-nav .drawer-menu-button'
+  );
+
+  function toggleDrawerMenu() {
+    const drawerMenu = this.parentNode;
+    drawerMenu.classList.toggle('is-open');
+  }
+
+  drawerMenuButtonList.forEach((button) => {
+    button.addEventListener('click', toggleDrawerMenu);
+  });
+});
